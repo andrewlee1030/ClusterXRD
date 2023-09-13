69658496fed6eb794a2e6b00a2367ba63de169a4
Andrew Lee <andrewlee@MacBook-Pro-3.local>
2023-09-13 12:52:14 -0500

    minor edits

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='clusterXRD',
+setup(name='ClusterXRD',
     version='1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires = ['matplotlib', 'scikit-learn', 'numpy','pandas'],
     author='Andrew S. Lee',
     author_email='andrewlee1030@gmail.com',
-    python_requires='>=3.7.0',
+    python_requires='>=3.9.0',
     )
