24372a4eeed0d5ab7ef2d133b030417108005c05
Andrew Lee <andrewlee@MacBook-Pro-3.local>
2023-09-14 09:55:23 -0500

    removed extra code

--- clusterXRD/utils/clustering_analysis.py
+++ clusterXRD/utils/clustering_analysis.py
@@ -88,24 +88,6 @@ def clustering_convergence_check(post_clustering_dir,cluster_dir):
     else: 
         np.savetxt(f'{post_clustering_dir}/reclustering_input',[delta_cluster_number])
 
-def get_similarity_stats(similarities,prefix):
-
-    try:
-
-        mean = np.mean(similarities)
-        median = np.median(similarities)
-        std = np.std(similarities)
-        minimum = np.min(similarities)
-        pctile_25 = np.quantile(similarities,0.25)
-
-        return {f'mean_{prefix}':mean, 
-                f'median_{prefix}':median,
-                f'std_{prefix}':std, 
-                f'min_{prefix}':minimum, 
-                f'pctile_25_{prefix}':pctile_25}
-    except:
-        return dict() # return empty dictionary when there are no phases in a cluster
-
 def get_similarity_matrix(cluster_features,cluster_dir,split_histograms_dir):
     '''
     Calculates pairwise similarities between histograms from each cluster.
@@ -146,37 +128,3 @@ def get_similarity_matrix(cluster_features,cluster_dir,split_histograms_dir):
 
         pd.DataFrame(data=cosine_similarities).set_axis(cluster_plot_names,axis=1).set_axis(cluster_plot_names,axis=0).to_csv(f'{cluster_dir}/similarity_scores/{int(cluster)}_cosine.csv')
  
-def plot_histogram(feature_set,title,across_all_wafers,has_crystalline, fig_dir = '.'): # unused
-    similarity_metrics = has_crystalline[feature_set].to_numpy().flatten()
-    similarity_metrics_no_nan = similarity_metrics[~np.isnan(similarity_metrics)]
-    if across_all_wafers: 
-        multiplier = 1.5
-        nbins=5
-    else: 
-        multiplier = 1
-        nbins=50
-    counts,bins = np.histogram(similarity_metrics_no_nan,bins=nbins)
-    center = (bins[:-1] + bins[1:])/2
-    width = multiplier * (bins[1] - bins[0])
-    plt.bar(center, counts, align='center', width=width)
-    plt.xlabel('Similarity Coefficient / Score')
-    plt.xlim([-0.1,1.0])
-    plt.ylabel('Number of Clusters')
-    if across_all_wafers: title += ' across all wafers'
-    plt.title(title)
-    plt.savefig(f'{fig_dir}/figs/{title}.png',dpi=300)
-    plt.close()
-
-def plot_histogram_per_wafer(similarities,title,fig_dir = None,nbins=20): # unused
-    counts,bins = np.histogram(similarities,bins=nbins)
-    center = (bins[:-1] + bins[1:])/2
-    width = (bins[1] - bins[0])
-    plt.bar(center, counts, align='center', width=width)
-    plt.xlabel('Similarity Coefficient / Score')
-    plt.xlim([-0.1,1.0])
-    plt.ylabel('Count')
-    plt.title(title)
-    if fig_dir == None: plt.show()
-    else:
-        plt.savefig(f'{fig_dir}/similarity_scores/{title}.png',dpi=300)
-        plt.close()
