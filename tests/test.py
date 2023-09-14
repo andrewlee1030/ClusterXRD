42e47ecf5588f43967448ad866aef185a8b5ebc5
Andrew Lee <andrewlee@MacBook-Pro-3.local>
2023-09-14 09:56:05 -0500

    changed parameters for new sample data

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
+import sys
+sys.path.append('../')
 from ClusterXRD import *
 
 if __name__ ==  '__main__':
     input_dir = 'cluster_test_files/cluster_1'
 
-    ezCluster(4,input_dir,'_bkgdsub_1D.csv',3)
+    ezCluster(4,input_dir,'_bkgdSub_1D.csv',5)
